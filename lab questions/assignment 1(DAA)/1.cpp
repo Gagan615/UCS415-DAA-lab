c5ea60174c4cbdb4e2c69085261498fee79364a5
Gagan615 <purba06gagan@gmail.com>
2026-01-20 00:32:17 +0530

    Add files via upload

---
+++ lab questions/assignment 1(DAA)/1.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int low, int high, int key) {
+    if (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] > key)
+            return binarySearch(arr, low, mid - 1, key);
+        else
+            return binarySearch(arr, mid + 1, high, key);
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int n = 10;
+    int key = 23;
+
+    int result = binarySearch(arr, 0, n - 1, key);
+
+    if (result != -1)
+        cout << "Element found at index: " << result << endl;
+    else
+        cout << "Element not found" << endl;
+
+    return 0;
+}
